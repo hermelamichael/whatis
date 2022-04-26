a2397708de7bb17e56f0b0ed40a7ec0adeb5e42e
hermelamichael <99145797+hermelamichael@users.noreply.github.com>
2022-04-26 11:03:34 -0500

    commit on "master"

---
+++ testing.py
@@ -0,0 +1,22 @@
+#Notes: Front end what the user interacts with, the visual elements of a website or app 
+#back end us the server side 
+#web development is the process if buolding websites and applications, front end and back end are the two types of web deleopments 
+#Front end development focuses on things that will be seen by users. Its all about making the 
+#visual aspects of a website functional 
+
+#back end development focuses on the part of a website that users cant see #aka server side 
+#the storage center for the server side is called a database (MySQL and SQL) database is run from a server 
+#front end development depends of back end development bc it ensures that front end development are working properly 
+#back end development works on storing and arranging data while ^
+
+#^ building code, troublshooting and debugging, database management 
+
+
+#front end development uses langauge like html(hyper text markup lang), css(cascading style sheet), and js(site interaction)
+#html is the standard markup language for creating webpages (structure )
+#css style, color and font,
+
+#back end three most common things like ruby, php Node.js and js is for both 
+
+
+#full stack is someone who works with both ^
